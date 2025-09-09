c3d82799c6890ee53c4ad3162cb968bc71c95624
Asril Ilham N.F <sselasa170@gmail.com>
2025-09-09 09:48:32 +0700

    CarTest.java

--- VehicleTest.java
+++
@@ -1,9 +0,0 @@
-public class VehicleTest {
-    public static void main(String[] args) {
-        Vehicle vehicle1 = new Vehicle();
-        vehicle1.print();
-
-        Vehicle vehicle2 = new Vehicle("Honda", 50);
-        vehicle2.print();
-    }
-}
